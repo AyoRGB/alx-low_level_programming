b2eb91360380656efdc3f6dc6c021cff3178acf2
AyoRGB <sekinatayodeji@gmail.com>
2022-06-30 21:49:06 -0700

    task 4-6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - prints the size of various types on the computer
+ *Return : returns the value 0 if exit properly, non-zero otherwise
+ */
+int main(void)
+{
+	int a;
+	long long int c;
+	char d;
+	float f;
+	long int b;
+
+	printf("size of a char : %lu byte(s)\n", size of(d));
+	printf("Size of an int : %lu byte(s)\n", size of(a));
+	printf("Size of an long int : %lu byte(s)\n", size of(b));
+	printf("Size of an long long int : %lu byte(s)\n", size of(c));
+	printf("Size of a float : %lu byte(s)\n", size of(int));
+	return (0);
+}
