85cd5505b00760642b8f8d1814783b0aba4c283a
AyoRGB <sekinatayodeji@gmail.com>
2022-07-03 21:12:20 -0700

    5 corrections

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,14 +3,14 @@
 /**
  * main - prints all single digit numbers of base ten,
  * followed by a new line
- * Return : 0 (success)
+ * Return : Always 0 (success)
  */
 int main(void)
 {
 	int i;
 
-	for (n = 0; n < 10: n++)
-		printf("%d", n);
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
 	printf("\n");
 	return (0);
 }
