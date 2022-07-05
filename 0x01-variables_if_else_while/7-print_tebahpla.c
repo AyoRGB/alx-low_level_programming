9304ca18a4d92e9e5968e3ac53225fec26ad1322
AyoRGB <sekinatayodeji@gmail.com>
2022-07-04 19:39:06 -0700

    tasks

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,18 @@
-int
+#include<stdio.h>
+
+/**
+ * main - prints lowercase alphabet in reverse,
+ * followed by a new line using the function putchar
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char ch;
+
+for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
